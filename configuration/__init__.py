249b7f7119d2f9969fbdf96e73c71a1718cce0e9
Alan Lee <alanleunglee@gmail.com>
2020-10-05 16:39:26 -0700

    feat(init): add init variables

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -1,5 +1,6 @@
 from app import app
 import urllib
+import os
 
 # secret key for user session
 app.secret_key = "ITSASECRET"
@@ -7,16 +8,16 @@ app.secret_key = "ITSASECRET"
 #setting up mail
 app.config['MAIL_SERVER']='' #mail server
 app.config['MAIL_PORT'] = 587 #mail port
-app.config['MAIL_USERNAME'] = '' #email
+app.config['MAIL_USERNAME'] = os.environ['MAIL_USERNAME']#email
 app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD'] #password
 app.config['MAIL_USE_TLS'] = True #security type
 app.config['MAIL_USE_SSL'] = False #security type
 
 #database connection parameters
 connection_params = {
-    'user': '',
+    'user': os.environ['DB_USERNAME'],
     'password': os.environ['DB_PASSWORD'],
-    'host': '',
-    'port': 'port',
+    'host': os.environ['DB_HOST'],
+    'port': os.environ['DB_PORT'],
     'namespace': '',
 }
